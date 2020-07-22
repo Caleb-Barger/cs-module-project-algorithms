36a91d09cd4bba34326885ced4b193639b01f2a8
caleb <bargercj20@hotmail.com>
2020-07-22 12:45:44 -0700

    single number solved recursivly

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,10 +3,23 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # base case(s)?
+    # the array has a len of 1
+    if len(arr) == 1:
+        return arr[0]
+    # the array has a len of 0
+    if len(arr) == 0:
+        return -1
 
-    pass
-
+    # if that number appears more than once
+    for i in range(1, len(arr)):
+        # check the current index agenst the first number
+        if arr[i] == arr[0]:
+            del arr[i]
+            # call self with new arr
+            return single_number(arr[1:])
+        else:
+            return single_number([arr[0]])
 
 if __name__ == '__main__':
     # Use the main function to test your implementation

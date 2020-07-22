110e2d5e6f622a5d5bef8a10e4086c50678bdfbd
caleb <bargercj20@hotmail.com>
2020-07-22 13:39:51 -0700

    moved the zeros!

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,11 +3,20 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
 
 
+    for i in range(len(arr)):
+        # wall is at 0
+        wall = i
+        
+        # iterate from current iterator + 1 to len(arr) - 1
+        for j in range(i+1, len(arr)):
+            if arr[j] == 0:
+                wall = j
+
+        arr[i], arr[wall] = arr[wall], arr[i]
+        
+    
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
